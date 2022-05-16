a894a529b3570b412a5a398e2debbb26c3c12436
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-16 15:22:42 -0300

    Viewing the repositories by getting the data from the GitHub API.

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/vargastm/be-the-hero'
-}
+import { useState, useEffect } from 'react'
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/vargastm/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   )
